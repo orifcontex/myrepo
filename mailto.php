2d66f61c648f2b7d2402806017f82055abc983e3
Orif <118767563+orifcontex@users.noreply.github.com>
2024-05-28 13:02:34 +0700

    Update mailto.php

--- mailto.php
+++ mailto.php
@@ -2,6 +2,6 @@
 $from = filter_var($_POST['from'], FILTER_VAR_EMAIL);
 // mailto will be here
 $to = 'email@example.com';
-$message ="Hello!";
+$message ="Hello world!";
 
 $res = mailto($from, $to, $message); 
